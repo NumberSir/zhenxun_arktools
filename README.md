d0dfcac89ab6c49acab1ccccd79bcc8df0de3cbd
Number_Sir <number_sir@126.com>
2022-06-04 09:47:54 +0800

    为公招推荐添加帮助信息

--- README.md
+++ README.md
@@ -21,3 +21,11 @@ _✨ ——的[真寻](https://github.com/HibiKier/zhenxun_bot)适配 ✨_
 ### 安装方法
 
 将 **arktools** 文件夹整个放进真寻 bot 的 **plugins** 文件夹即可
+
+### 帮助指令
+
+> 帮助 方舟今日资源
+> 
+> 帮助 方舟最新活动
+> 
+> 帮助 方舟公招推荐
--- arktools/open_recruitment/__init__.py
+++ arktools/open_recruitment/__init__.py
@@ -1,9 +1,29 @@
+"""公招一四五六星推荐"""
 from nonebot import on_command
 from nonebot.adapters.onebot.v11 import Message
 from nonebot.params import CommandArg
 
 from services.log import logger
 from .data_source import get_recommend_tags
+
+__zx_plugin_name__ = "方舟公招推荐"
+__plugin_usage__ = """
+usage：
+    输入公招tag来看看哪些组合会出一四五六星干员
+    指令：
+        公开招募 [标签1 标签2 ...]
+""".strip()
+__plugin_des__ = "输入公招tag来看看哪些组合会出一四五六星干员"
+__plugin_cmd__ = ["公开招募"]
+__plugin_type__ = ("方舟相关",)
+__plugin_version__ = 0.2
+__plugin_author__ = "Number_Sir"
+__plugin_settings__ = {
+    "level": 5,
+    "default_status": True,
+    "limit_superuser": False,
+    "cmd": ["公开招募"],
+}
 
 recruit = on_command("公开招募", aliases={"公招", "方舟公招", "公招tag"}, priority=5, block=True)
 
